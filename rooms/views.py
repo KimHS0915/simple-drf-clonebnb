0489d1e16b139acb81f958f16473e4034de9d20e
KimHS0915 <remember2552@gmail.com>
2022-07-08 16:51:38 +0900

    Add PhotoSerializer and Fix room search

--- rooms/views.py
+++ rooms/views.py
@@ -32,20 +32,20 @@ class RoomViewSet(ModelViewSet):
         lng = request.GET.get("lng", None)
         filter_kwargs = {}
         if max_price is not None:
-            filter_kwargs["price_lte"] = max_price
+            filter_kwargs["price__lte"] = max_price
         if min_price is not None:
-            filter_kwargs["price_gte"] = min_price
+            filter_kwargs["price__gte"] = min_price
         if beds is not None:
-            filter_kwargs["beds_gte"] = beds
+            filter_kwargs["beds__gte"] = beds
         if bedrooms is not None:
-            filter_kwargs["bedrooms_gte"] = bedrooms
+            filter_kwargs["bedrooms__gte"] = bedrooms
         if bathrooms is not None:
-            filter_kwargs["bathrooms_gte"] = bathrooms
+            filter_kwargs["bathrooms__gte"] = bathrooms
         if lat is not None and lng is not None:
-            filter_kwargs["lat_gte"] = float(lat) - 0.005
-            filter_kwargs["lat_lte"] = float(lat) + 0.005
-            filter_kwargs["lng_gte"] = float(lng) - 0.005
-            filter_kwargs["lng_lte"] = float(lng) + 0.005
+            filter_kwargs["lat__gte"] = float(lat) - 0.005
+            filter_kwargs["lat__lte"] = float(lat) + 0.005
+            filter_kwargs["lng__gte"] = float(lng) - 0.005
+            filter_kwargs["lng__lte"] = float(lng) + 0.005
         paginator = self.paginator
         try:
             rooms = Room.objects.filter(**filter_kwargs)
